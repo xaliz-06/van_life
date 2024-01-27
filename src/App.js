67aeabc960db197724244c2d7ba55cad80696510
xaliz-06 <vaibhavchakraborty06@gmail.com>
2024-01-27 15:18:48 +0530

    fixed minor bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Root from './routes/Root';
 import Home from './routes/Home';
